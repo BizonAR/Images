466c379cf3f54a97b42f37920bddd9b806ac326c
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 00:24:26 +0500

    Images

---
+++ Images.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Images
+{
+	internal class Images
+	{
+		static void Main(string[] args)
+		{
+			int numberPictures = 52;
+			int picturesInRow = 3;
+			int filledRows;
+			int extraPictures;
+
+			filledRows = numberPictures / picturesInRow;
+			extraPictures = numberPictures % picturesInRow;
+
+			Console.WriteLine($"Заполненных рядов: {filledRows}, картинок сверх меры: {extraPictures}");
+		}
+	}
+}
